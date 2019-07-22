445e526b488cf34f68f808f7658017e791527419
Eshan Jain <37260065+eshanja1n@users.noreply.github.com>
2019-07-22 14:03:35 -0700

    Update mail.php

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Message: $message";
 $recipient = "eshanjain@gmail.com";
-$subject = "BAY Summer";
+$subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
