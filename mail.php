e6da4ec94b99a7c60a0d22025ef03dd03698d248
Eshan Jain <37260065+eshanja1n@users.noreply.github.com>
2019-07-22 13:49:11 -0700

    contact page

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+
+$message = $_POST['message'];
+$formcontent=" From: $name \n Subject: $subject \n Message: $message";
+$recipient = "eshanjain@gmail.com";
+$subject1 = "BAY Summer Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject1, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
