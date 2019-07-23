7a32d69dfb92a1267004047e05b404e40bea0772
Eshan Jain <eshanjain@gmail.com>
2019-07-22 23:15:12 -0700

    jj

--- mail.php
+++ mail.php
@@ -8,5 +8,5 @@ $recipient = "eshanjain@gmail.com";
 $subject = "BAY Summer Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You for you message!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Go Back</a>";
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
